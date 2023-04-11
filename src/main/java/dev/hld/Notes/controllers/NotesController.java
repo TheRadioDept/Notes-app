964a8be4f606a2716d08b9111e1b55a3d339e5bb
TheRadioDept <farkhadmaratov99@gmail.com>
2023-04-11 17:41:34 +0400

    Configured GET, PUT, and DELETE requests

--- src/main/java/dev/hld/Notes/controllers/NotesController.java
+++ src/main/java/dev/hld/Notes/controllers/NotesController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import dev.hld.Notes.models.Note;
 import dev.hld.Notes.models.User;
 import dev.hld.Notes.repositories.NotesRepository;
 import dev.hld.Notes.repositories.UserRepository;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -40,6 +42,22 @@ public class NotesController {
         return new ResponseEntity<>(notes, HttpStatus.OK);
     }
 
+    @GetMapping("/notes")
+    Iterable<Note> getNotes() {
+        return notesRepository.findAll();
+    }
+
+    @GetMapping("/users/{userId}/notes/{noteId}")
+    public ResponseEntity<Note> getSingleNote(@PathVariable(value = "userId") String userId,
+            @PathVariable(value = "noteId") String noteId) throws RelationNotFoundException {
+        userRepository.findById(userId)
+                .orElseThrow(() -> new RelationNotFoundException("Not found User with id = " + userId));
+        Note note = notesRepository.findById(noteId)
+                .orElseThrow(() -> new RelationNotFoundException("Not found User with id = " + noteId));
+        ;
+        return new ResponseEntity<>(note, HttpStatus.OK);
+    }
+
     @PostMapping("/users/{userId}/notes")
     public ResponseEntity<Note> createNote(@PathVariable(value = "userId") String userId, @RequestBody Note noteRequest)
             throws RelationNotFoundException {
@@ -51,4 +69,21 @@ public class NotesController {
         return new ResponseEntity<>(notes, HttpStatus.OK);
     }
 
+    @PutMapping("/users/{userId}/notes/{id}")
+    public ResponseEntity<Note> updateNote(@PathVariable("id") String id, @PathVariable("userId") String userId,
+            @RequestBody Note commentRequest) throws RelationNotFoundException {
+        userRepository.findById(userId)
+                .orElseThrow(() -> new RelationNotFoundException("Not found User with id = " + userId));
+        Note comment = notesRepository.findById(id)
+                .orElseThrow(() -> new RelationNotFoundException("CommentId " + id + "not found"));
+
+        comment.setNoteBody(commentRequest.getNoteBody());
+
+        return new ResponseEntity<>(notesRepository.save(comment), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/notes/{id}")
+    void deleteNote(@PathVariable String id) {
+        notesRepository.deleteById(id);
+    }
 }

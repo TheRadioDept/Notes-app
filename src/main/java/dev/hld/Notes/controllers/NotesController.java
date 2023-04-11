820615a95d265aa7171fa5b12468c53d0e6e5902
TheRadioDept <farkhadmaratov99@gmail.com>
2023-04-11 15:40:04 +0400

    Added OneToMany relation between User and Notes

--- src/main/java/dev/hld/Notes/controllers/NotesController.java
+++ src/main/java/dev/hld/Notes/controllers/NotesController.java
@@ -1,37 +1,52 @@
 package dev.hld.Notes.controllers;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.relation.RelationNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.hld.Notes.models.Notes;
+import dev.hld.Notes.models.Note;
+import dev.hld.Notes.models.User;
 import dev.hld.Notes.repositories.NotesRepository;
+import dev.hld.Notes.repositories.UserRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class NotesController {
 
     @Autowired
     private NotesRepository notesRepository;
 
-    @GetMapping("/notes")
-    Iterable<Notes> getNotes() {
-        return notesRepository.findAll();
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users/{userId}/notes")
+    public ResponseEntity<List<Note>> getAllNotesByUserId(@PathVariable(value = "userId") String userId) throws RelationNotFoundException {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new RelationNotFoundException("Not found User with id = " + userId));
+        List<Note> notes = new ArrayList<Note>();
+        notes.addAll(user.getNote());
+        return new ResponseEntity<>(notes, HttpStatus.OK);
     }
 
-    @GetMapping("/notes/{id}")
-    Optional<Notes> getNoteById(@PathVariable String noteId) {
-        return notesRepository.findById(noteId);
-    }
+    @PostMapping("/users/{userId}/notes")
+    public ResponseEntity<Note> createNote(@PathVariable(value = "userId") String userId, @RequestBody Note noteRequest) throws RelationNotFoundException {
+        Note notes = userRepository.findById(userId).map(user -> {
+            user.getNote().add(noteRequest);
+            return notesRepository.save(noteRequest);
+        }).orElseThrow(() -> new RelationNotFoundException("Not found User with id = " + userId));
 
-    @PostMapping("/notes/add")
-    Notes addNote(@RequestParam String note_body) {
-        Notes note = new Notes(note_body);
-        return notesRepository.save(note);
+        return new ResponseEntity<>(notes, HttpStatus.OK);
     }
 
 }
--- src/main/java/dev/hld/Notes/repositories/UserRepository.java
+++ src/main/java/dev/hld/Notes/repositories/UserRepository.java
@@ -1,7 +1,13 @@
 package dev.hld.Notes.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import dev.hld.Notes.models.User;
 
-public interface UserRepository extends CrudRepository<User, String> {}
+public interface UserRepository extends CrudRepository<User, String> {
+   User findByUserId(String userId);
+
+    List<User> findByUserName(String userName);
+}
